bdf9174174c2dc624e0855cbb584c0c04b750748
rfox06 <ger_ly06@keemail.me>
2023-01-03 23:44:53 -0500

    new

---
+++ js/modal.js
@@ -0,0 +1,46 @@
+
+let images = document.querySelectorAll('.gallery__img');
+let modal = document.querySelector('#modal');
+let img = document.querySelector('#modal__img');
+let boton = document.querySelector('#modal__boton');
+
+for (let i = 0; i<images.length; i++){
+    images[i].addEventListener('click', function(e){
+        modal.classList.toggle("modal--open");
+        let src = e.target.src;
+        img.setAttribute("src", src);
+    });
+} 
+
+boton.addEventListener('click', function(){
+    modal.classList.toggle("modal--open");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
